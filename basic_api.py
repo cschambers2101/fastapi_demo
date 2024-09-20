c7397ab2d45dfb8bdba1126f099f9711812b9dea
Craig Chambers <cchambers@s6c.mlp.college>
2024-09-20 13:41:32 +0100

    Added comments

--- basic_api.py
+++ basic_api.py
@@ -5,6 +5,7 @@ from sqlmodel import create_engine, Session, SQLModel, Field, select
 
 
 class Students(SQLModel, table=True):
+    ''' This class is used to create a table in the database for students'''
     __tablename__ = "students"
     id: Optional[int] = Field(default=None, primary_key=True)
     firstname: str
@@ -12,20 +13,24 @@ class Students(SQLModel, table=True):
 
 
 class Student(SQLModel):
+    ''' This class is used to create to map a response for a student'''
     id: Optional[int] = Field(default=None, primary_key=True)
     firstname: str
     lastname: str
 
 
+# Setup the database
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 engine = create_engine(sqlite_url, echo=True)
 
+# Create the database and all tables
 SQLModel.metadata.create_all(engine)
 
+# Create the FastAPI app
 app = FastAPI()
 
-
+# Add CORS middleware to the app
 origins = [
     "http://localhost",
     "http://localhost:8080",
@@ -43,6 +48,7 @@ app.add_middleware(
 
 @app.post("/students", response_model=Students)
 async def create_student(student: Students):
+    ''' This function is used to create a student in the database'''
     with Session(engine) as db:
         db.add(student)
         db.commit()
@@ -52,6 +58,7 @@ async def create_student(student: Students):
 
 @app.get("/students", response_model=List[Students])
 async def read_students():
+    ''' This function is used to get all students in the database'''
     with Session(engine) as db:
         statement = select(Students)
         students = db.exec(statement).all()
@@ -60,6 +67,7 @@ async def read_students():
 
 @app.get("/students/{student_id}", response_model=Student)
 async def read_student(student_id: int):
+    ''' This function is used to get a student by id in the database'''
     with Session(engine) as db:
         statement = select(Students).where(Students.id == student_id)
         student = db.exec(statement).first()
@@ -68,6 +76,7 @@ async def read_student(student_id: int):
 
 @app.delete("/students/{student_id}", status_code=204)
 async def delete_student(student_id: int):
+    ''' This function is used to delete a student by id in the database'''
     with Session(engine) as db:
         statement = select(Students).where(Students.id == student_id)
         student = db.exec(statement).first()
@@ -78,6 +87,7 @@ async def delete_student(student_id: int):
 
 @app.patch("/students/{student_id}", response_model=Student)
 async def update_student(student_id: int, student: Student):
+    ''' This function is used to update a student by id in the database'''
     with Session(engine) as db:
         statement = select(Students).where(Students.id == student_id)
         student_db = db.exec(statement).first()
